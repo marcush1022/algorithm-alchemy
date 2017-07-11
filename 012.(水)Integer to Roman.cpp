6db40d6a2eb9a745a4c43f95052b948a87500e39
Marcus Skywalker <marcush1022@163.com>
2017-07-11 10:42:07 +0800

    Update and rename 012.(水)Integer to Roman to 012.(水)Integer to Roman.cpp

--- 012.(水)Integer to Roman.cpp
+++ 012.(水)Integer to Roman.cpp
@@ -9,25 +9,25 @@ public:
         string str="";
         if(num<=0 || num>3999)
             return str;
-		int x=num;
+	int x=num;
 
         char* digit[10]={"","I","II","III","IV","V","VI","VII","VIII","IX"};
-		char* ten[10]={"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-		char* hundred[10]={"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-		char* thousand[4]={"","M","MM","MMM"};
+	char* ten[10]={"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+	char* hundred[10]={"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+	char* thousand[4]={"","M","MM","MMM"};
 
-		int di,te,hu,th;
-		di=num%10;
-		te=num%100/10;
-		hu=num%1000/100;
-		th=num/1000;
+	int di,te,hu,th;
+	di=num%10;
+	te=num%100/10;
+	hu=num%1000/100;
+	th=num/1000;
 
 
-		str+=thousand[th];
-		str+=hundred[hu];
-		str+=ten[te];
-		str+=digit[di];
+	str+=thousand[th];
+	str+=hundred[hu];
+	str+=ten[te];
+	str+=digit[di];
 
-		return str;
+	return str;
     }
 };
