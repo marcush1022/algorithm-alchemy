8d7f71c667ecc936c67d7ced709e259bd04f7966
Marcus Skywalker <marcush1022@163.com>
2017-07-11 10:11:19 +0800

    Update 021.(链表操作)Merge Two Sorted Lists.cpp

--- 021.(链表操作)Merge Two Sorted Lists.cpp
+++ 021.(链表操作)Merge Two Sorted Lists.cpp
@@ -44,3 +44,26 @@ public:
 		return head;
     }
 };
+
+递归：
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *head;
+		if(l1==NULL)
+            return l2;
+        if(l2==NULL)
+            return l1;
+        if(l1->val<l2->val)
+        {
+            head=l1;
+            head->next=mergeTwoLists(l1->next, l2);
+        }
+        else
+        {
+            head=l2;
+            head->next=mergeTwoLists(l1, l2->next);
+        }
+        return head;
+    }
+};
