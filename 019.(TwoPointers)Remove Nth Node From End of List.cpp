0bbeb3ef83e925e3720733724081f03b0660d013
Marcus Skywalker <marcush1022@163.com>
2017-07-11 10:44:43 +0800

    Update and rename 019.(TwoPointers)Remove Nth Node From End of List to 019.(TwoPointers)Remove Nth Node From End of List.cpp

--- 019.(TwoPointers)Remove Nth Node From End of List.cpp
+++ 019.(TwoPointers)Remove Nth Node From End of List.cpp
@@ -15,27 +15,26 @@ class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         if(head==NULL)
-			return head;
-		ListNode *left, *right;
-		ListNode *p;
-		left=right=head;
-		int i=0;
-		while(i<n)
-		{
-			++i;
-			right=right->next;
-		}
-		//right为空说明要删的节点为倒数n个即第一个
-        	if(right==NULL)
-            		return head->next;
-		while(right->next!=NULL)
-		{
-			right=right->next;
-			left=left->next;
-		}
-		left->next=left->next->next;
-		
 		return head;
+	ListNode *left, *right;
+	ListNode *p;
+	left=right=head;
+	int i=0;
+	while(i<n)
+	{
+		++i;
+		right=right->next;
+	}
+	//right为空说明要删的节点为倒数n个即第一个
+        if(right==NULL)
+            	return head->next;
+	while(right->next!=NULL)
+	{
+		right=right->next;
+		left=left->next;
+	}
+	left->next=left->next->next;
+	return head;
     }
 };
 
