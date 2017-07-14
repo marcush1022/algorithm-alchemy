5135dc816273f7e65bdb390da2fab58bc54a7448
Marcus Skywalker <marcush1022@163.com>
2017-07-14 09:30:40 +0800

    Update and rename 050.Pow(x, n) to 050.(分治法)Pow(x, n).cpp

---
+++ 050.(分治法)Pow(x, n).cpp
@@ -0,0 +1,63 @@
+/*
+Implement pow(x, n).
+
+分治解法：
+分：将n分成n/2  直到n=0时，返回1；
+治：对n为偶数，返回两数相乘的结果，奇数再乘多一个x
+
+注意当x=INT_MIN时，反过来-n会越界
+*/
+
+class Solution
+{
+	public:
+	double myPow(double x, int n)
+	{
+		double res=1;
+        if(x==1 || n==0)
+            return 1.0;
+        if(n<0)
+        {
+            if(n==INT_MIN)
+            {
+                n=INT_MAX;
+                res*=1.0/x;
+                x=1.0/x;
+            }
+            else
+            {
+                n*=-1;
+                x=1.0/x;
+            }
+        }
+		while(n>0)
+		{
+			if(n%2)
+                res=res*x;
+			n=n>>1;
+			x*=x;
+		}
+		return res;
+	}
+};
+
+递归:
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0 || x==1)
+            return 1.0;
+        if(n<0)
+        {
+            if(n==INT_MIN)
+                return 1.0/(myPow(x, INT_MAX))*x;
+            else
+                return 1.0/(myPow(x, -n));
+        }
+        double half=myPow(x, n>>1);
+        if(n%2)
+            return half*half*x;
+        else
+            return half*half;
+    }
+};
