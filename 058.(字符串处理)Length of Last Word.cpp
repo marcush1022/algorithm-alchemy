6180bcc830a5d4b1941fef68fafef68e526d5d84
Marcus Skywalker <marcush1022@163.com>
2017-07-18 20:55:49 +0800

    Update 058.(字符串处理)Length of Last Word.cpp

--- 058.(字符串处理)Length of Last Word.cpp
+++ 058.(字符串处理)Length of Last Word.cpp
@@ -1,4 +1,4 @@
-/***************************************************************************************/
+/*
 Given a string s consists of upper/lower-case alphabets and empty space characters ' ', 
 return the length of last word in the string.
 
@@ -9,7 +9,7 @@ Note: A word is defined as a character sequence consists of non-space characters
 For example, 
 Given s = "Hello World",
 return 5.
-/***************************************************************************************/
+*/
 
 class Solution {
 public:
