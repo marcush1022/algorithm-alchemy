b52fcf4e494b91bc0e10471631c68544ead3df53
Marcus Skywalker <marcush1022@163.com>
2017-07-19 20:31:47 +0800

    Update 064.(动规)Minimum Path Sum.cpp

--- 064.(动规)Minimum Path Sum.cpp
+++ 064.(动规)Minimum Path Sum.cpp
@@ -32,3 +32,29 @@ public:
         return arr[n-1];
     }
 };
+
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m=grid.size(), n=grid[0].size();
+        if(m==0 || n==0)
+            return 0;
+
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(i==0 && j==0)
+                    continue;
+                else if(i==0)
+                    grid[i][j]+=grid[i][j-1];
+                else if(j==0)
+                    grid[i][j]+=grid[i-1][j];
+                else
+                    grid[i][j]+=min(grid[i][j-1], grid[i-1][j]);
+            }
+        }
+        return grid[m-1][n-1];
+    }
+};
