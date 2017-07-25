cd52e8b61cd722b155640ea79f5ca8f662cb7951
Marcus Skywalker <marcush1022@163.com>
2017-07-25 21:23:30 +0800

    Update and rename 082.(Two Pointers)Remove Duplicates from Sorted List II to 082.(链表操作)Remove Duplicates from Sorted List II.cpp

--- 082.(链表操作)Remove Duplicates from Sorted List II.cpp
+++ 082.(链表操作)Remove Duplicates from Sorted List II.cpp
@@ -1,37 +1,35 @@
-/************************************************************************************/
+/*
 Given a sorted linked list, delete all nodes that have duplicate numbers, 
 leaving only distinct numbers from the original list.
 
 For example,
 Given 1->2->3->3->4->4->5, return 1->2->5.
 Given 1->1->1->2->3, return 2->3.
-/************************************************************************************/
+*/
 
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         if(head==NULL || head->next==NULL)
             return head;
-        ListNode *pre, *p;
         ListNode *nhead=new ListNode(0);
         nhead->next=head;
-        pre=nhead, p=nhead->next;
+        ListNode *pre=nhead, *p=nhead->next;
         while(p)
         {
-            while(p->next!=NULL && p->next->val==pre->next->val)
+            while(p->next!=NULL && pre->next->val==p->next->val)
                 p=p->next;
-            if(pre->next==p)
-            {
-                pre=p;
-                p=p->next;
-            }
-            else
+            if(pre->next!=p)
             {
                 pre->next=p->next;
                 p->next=NULL;
                 p=pre->next;
             }
-            
+            else
+            {
+                pre=p;
+                p=p->next;
+            }
         }
         return nhead->next;
     }
