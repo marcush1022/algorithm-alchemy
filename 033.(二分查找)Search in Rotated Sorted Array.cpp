81e7c426bd5f5f3fd5a8af92747757ad8e5b80f4
Marcus Skywalker <marcush1022@163.com>
2017-07-25 21:04:36 +0800

    Update 033.(二分查找)Search in Rotated Sorted Array.cpp

--- 033.(二分查找)Search in Rotated Sorted Array.cpp
+++ 033.(二分查找)Search in Rotated Sorted Array.cpp
@@ -8,38 +8,41 @@ You are given a target value to search. If found in the array return its index, 
 You may assume no duplicate exists in the array.
 
 旋转数组分2种情况：
-1. 最左大于中间
-2. 最左小于中间
-旋转数组与普通数组二分查找的区别是只需要进行一半的查找，主要难点在边界值处理
+1. 最左大于等于中间
+2. 最左小于等于中间
+旋转数组与普通数组二分查找的区别是只需要进行一半的查找
+注意11131和13111的情况，需要left++跳过重复数字
 */
 
 class Solution {
 public:
-    int search(vector<int>&nums, int target) {
+    int search(vector<int>& nums, int target) {
         int len=nums.size();
         if(len==0)
             return -1;
-        int left=0, right=len, mid=0;
+        int left=0, right=len-1;
         while(left<right)
         {
-            mid=(left+right)/2;
+            int mid=(left+right)/2;
             if(nums[mid]==target)
                 return mid;
-            if(nums[mid]>=nums[left])
+            if(nums[mid]<nums[left])
             {
-                if(nums[left]<=target && nums[mid]>target)
-                    right=mid;
-                else
-                    left=mid+1;
-            }
-            else if(nums[mid]<=nums[left])
-            {
-                if(nums[mid]<target && nums[right-1]>=target)
+                if(nums[mid]<target && nums[right]>=target)
                     left=mid+1;
                 else
                     right=mid;
             }
+            else if(nums[mid]>nums[left])
+            {
+                if(nums[mid]>target && nums[left]<=target)
+                    right=mid;
+                else
+                    left=mid+1;
+            }
+            else
+                left++;
         }
-        return -1;
+        return nums[left]==target? left : -1;
     }
 };
