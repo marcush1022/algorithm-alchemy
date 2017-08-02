9a01e43784fdebed2d77e568f7eff627fe60437d
Marcus Skywalker <marcush1022@163.com>
2017-08-02 21:07:17 +0800

    Update and rename 121.(DP,水)Best Time to Buy and Sell Stock to 121.(DP,水)Best Time to Buy and Sell Stock.cpp

--- 121.(DP,水)Best Time to Buy and Sell Stock.cpp
+++ 121.(DP,水)Best Time to Buy and Sell Stock.cpp
@@ -1,4 +1,4 @@
-/**************************************************************************************/
+/*
 Say you have an array for which the ith element is the price of a given stock on day i.
 
 If you were only permitted to complete at most one transaction (ie, buy one and sell one
@@ -15,21 +15,18 @@ Input: [7, 6, 4, 3, 1]
 Output: 0
 
 In this case, no transaction is done, i.e. max profit = 0.
-/**************************************************************************************/
+*/
 
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int len=prices.size();
-		if(len==0)
-			return 0;
-		int minPri=INT_MAX, maxPro=0;
-		for(int i=0; i<len; i++)
-		{
-			if(prices[i]<minPri)
-				minPri=prices[i];
-			maxPro=std::max(maxPro, prices[i]-minPri);
-		}
-		return maxPro;
+        int maxPro=0, minPri=INT_MAX;
+        for(int i=0; i<prices.size(); i++)
+        {
+            if(minPri>prices[i])
+                minPri=prices[i];
+            maxPro=max(maxPro, prices[i]-minPri);
+        }
+        return maxPro;
     }
 };
