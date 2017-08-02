737769b1fa8ca2e7e1f0473f504c42ab94e888e7
Marcus Skywalker <marcush1022@163.com>
2017-08-02 21:40:37 +0800

    Update and rename 124.(树形动态规划)Binary Tree Maximum Path Sum to 124.(树形动规,dfs)Binary Tree Maximum Path Sum.cpp

--- 124.(树形动规,dfs)Binary Tree Maximum Path Sum.cpp
+++ 124.(树形动规,dfs)Binary Tree Maximum Path Sum.cpp
@@ -1,4 +1,4 @@
-/******************************************************************************************************/
+/*
 Given a binary tree, find the maximum path sum.
 
 For this problem, a path is defined as any sequence of nodes from some starting node to any node in 
@@ -15,31 +15,33 @@ Return 6.
 
 最大path sum指的是当前结点的值加左右到叶结点的路径和的最大值，用maxSum记录全局最大值
 例如：[1,2,3,4,5,6]最大path sum为17
-/******************************************************************************************************/
+
+*/
 
 class Solution {
-int maxSum=0;
 public:
+    int maxSum=0;  //maxSum记录全局最大值
+    
     int dfs(TreeNode *root)
-	{
-		if(root==NULL)
-			return 0;
-		int leftSum=dfs(root->left);
-		int rightSum=dfs(root->right);
-		
-		leftSum=max(leftSum, 0);
-		rightSum=max(rightSum, 0);
-		
-		int sum=leftSum+rightSum+root->val;
-		
-		maxSum=max(maxSum, sum);
-		
-		return root->val+max(leftSum, rightSum);
-	}
+    {
+        if(root==NULL)
+            return 0;
 
+        int leftSum=dfs(root->left);
+        int rightSum=dfs(root->right);
+        
+        leftSum=max(leftSum, 0);   //因为题目中路径的定义不一定从叶子开始
+        rightSum=max(rightSum, 0);
+        
+        int sum=leftSum+rightSum+root->val;
+        maxSum=max(maxSum, sum);
+        
+        return root->val+max(leftSum, rightSum);  //递归调用返回左右路径和的较大值
+    }
+    
     int maxPathSum(TreeNode* root) {
         maxSum=root->val;
         dfs(root);
-		return maxSum;
+        return maxSum;
     }
 };
