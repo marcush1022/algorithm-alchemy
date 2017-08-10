9daa8fde18c396ee1e230c61e5957682aa0a3ca2
Marcus Skywalker <marcush1022@163.com>
2017-08-10 20:22:43 +0800

    Update and rename 136.(Binary)Single Number to 136.(位运算,水)Single Number.cpp

--- 136.(位运算,水)Single Number.cpp
+++ 136.(位运算,水)Single Number.cpp
@@ -1,16 +1,17 @@
-/*******************************************************************************************/
+/*
 Given an array of integers, every element appears twice except for one. Find that single one.
 
 Note:
 Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
-/*******************************************************************************************/
+*/
 
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        int len=nums.size();
         int XOR=0;
-        for(int i=0; i<nums.size(); i++)
-        XOR^=nums[i];
+        for(int i=0; i<len; i++)
+            XOR^=nums[i];
         return XOR;
     }
 };
