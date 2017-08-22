00a426c87d2f47761fa127c361b5007c9f82fbf2
Marcus Skywalker <marcush1022@163.com>
2017-08-22 14:10:09 +0800

    Update and rename 172.(Math)Factorial Trailing Zeroes to 172.(Math)Factorial Trailing Zeroes.cpp

--- 172.(Math)Factorial Trailing Zeroes.cpp
+++ 172.(Math)Factorial Trailing Zeroes.cpp
@@ -1,4 +1,4 @@
-/*****************************************************************************************/
+/*
 Given an integer n, return the number of trailing zeroes in n!.
 
 Note: Your solution should be in logarithmic time complexity.
@@ -6,20 +6,21 @@ Note: Your solution should be in logarithmic time complexity.
 对n!做质因数分解n!=2x*3y*5z*...
 显然0的个数等于2*5的个数即min(x,z)，并且min(x,z)=z (归纳法证明)
 
-Note: Z = N/5 + N /(5 * 5) + N/( 5 * 5 * 5).....直到N/(5的K次方)等于0
+即：求不大于N的数中因子5、25、...的数量
+公式: Z = N/5 + N /(5 * 5) + N/( 5 * 5 * 5).....直到N/(5的K次方)等于0
 公式中N/5表示不大于N的数中能被5整除的数贡献一个5，N/(5 * 5)表示不大于N的数中能被25整除的数再共享
 一个5
-/*****************************************************************************************/
+*/
 
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int ret=0;
+        int count=0;
         while(n)
         {
-            ret+=n/5;
+            count+=n/5;
             n=n/5;
         }
-        return ret;
+        return count;
     }
 };
