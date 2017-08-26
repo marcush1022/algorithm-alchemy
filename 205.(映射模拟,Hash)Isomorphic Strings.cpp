99a5fcbc4a779ef94bfbfb3fd594ea2470a689d1
Marcus Skywalker <marcush1022@163.com>
2017-08-26 10:11:45 +0800

    Update and rename 205.(映射模拟,Hash)Isomorphic Strings to 205.(映射模拟,Hash)Isomorphic Strings.cpp

--- 205.(映射模拟,Hash)Isomorphic Strings.cpp
+++ 205.(映射模拟,Hash)Isomorphic Strings.cpp
@@ -1,4 +1,4 @@
-/***************************************************************************************************/
+/*
 Given two strings s and t, determine if they are isomorphic.
 
 Two strings are isomorphic if the characters in s can be replaced to get t.
@@ -15,34 +15,36 @@ Given "paper", "title", return true.
 
 Note:
 You may assume both s and t have the same length.
-/***************************************************************************************************/
+
+有以下两种情况的不是映射
+一对多:
+f--b
+o--a
+o--r
+
+多对一:
+b--f
+a--o
+r--o
+*/
 
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int len1=s.length();
-		int len2=t.length();
-		if(len1!=len2)
-			return false;
-		
-		vector<char> map(300);
-		vector<char> visited(300);
-		
-		for(int i=0; i<len1; i++)
-		{
-			if(map[s[i]])
-			{
-				if(map[s[i]]!=t[i])
-					return false;
-			}
-			else
-			{
-				if(visited[t[i]])
-					return false;
-			}
-			map[s[i]]=t[i];
-			visited[t[i]]=1;
-		}
-		return true;
+        int len1=s.length(), len2=t.length();
+        if(len1!=len2)
+            return false;
+        vector<char> map(300);
+        vector<char> visited(300);
+        for(int i=0; i<len1; i++)
+        {
+            if(map[s[i]] && map[s[i]]!=t[i]) //一对多
+                return false;
+            if(!map[s[i]] && visited[t[i]]) //多对一
+                return false;
+            map[s[i]]=t[i];
+            visited[t[i]]=1;
+        }
+        return true;
     }
 };
