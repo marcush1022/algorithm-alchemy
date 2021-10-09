f9ce1baf0d799fbd21de3eb149e2295bdaab3fea
marcush <wasd666@live.com>
2021-10-09 16:38:25 +0800

    add

--- golang/008.(字符串处理)String_to_Integer(atoi).go
+++ golang/008.(字符串处理)String_to_Integer(atoi).go
@@ -19,8 +19,6 @@ input specs). You are responsible to gather all the input requirements up
 front.
 */
 
-const intSize = 32 << (^uint(0) >> 63)
-
 var ErrSyntax = errors.New("invalid syntax")
 
 func isSpace(ch byte) bool {
@@ -30,64 +28,51 @@ func isSpace(ch byte) bool {
 	return false
 }
 
-func isDigit(ch byte) bool {
-	ch -= '0'
-	if ch > 9 {
-		fmt.Println(ErrSyntax, "ch > 9")
-		return false
-	}
-	return true
-}
-
 func MyAtoi(str string) int {
 	sLen := len(str)
+	if sLen == 0 {
+		return 0
+	}
 	index := 0
+	var sign byte
+
 	// skip leading spaces
 	for index < len(str) && isSpace(str[index]) {
 		index++
 	}
-	sign := str[index]
 
-	// fast path for small integers that fit int type
-	if intSize == 32 && (0 < sLen && sLen < 10) || intSize == 64 && (0 < sLen && sLen < 19) {
-		// deal +-
-		if str[index] == '-' || str[index] == '+' {
-			index++
-		}
+	// deal +-
+	if str[index] == '-' || str[index] == '+' {
+		sign = str[index]
+		index++
+	}
 
-		str = str[index:]
-		if len(str) < 1 {
-			fmt.Println(ErrSyntax, "len(str) < 1")
-			return 0
-		}
+	str = str[index:]
+	if len(str) < 1 {
+		fmt.Println(ErrSyntax, "len(str) < 1")
+		return 0
+	}
 
-		sum := 0
-		for _, ch := range []byte(str) {
-			ch -= '0'
-			if ch > 9 {
-				break
-			}
-			sum = sum*10 + int(ch)
-			fmt.Println(">>>>>>>>>>>> sum", sum, int(ch), str)
+	sum := 0
+	for _, ch := range []byte(str) {
+		ch -= '0'
+		if ch > 9 {
+			break
 		}
-
-		if sign == '-' {
-			sum = -sum
-		}
-		fmt.Println(">>>>>>>>>>>> results", sum)
-		if sum > 2147483647 {
-			return 2147483647
-		}
-
-		if sum < -2147483648 {
-			return -2147483648
-		}
-		return sum
+		sum = sum*10 + int(ch)
+		fmt.Println(">>>>>>>>>>>> sum", sum, int(ch), str)
 	}
 
 	if sign == '-' {
+		sum = -sum
+	}
+	fmt.Println(">>>>>>>>>>>> results", sum)
+	if sum > 2147483647 {
+		return 2147483647
+	}
+
+	if sum < -2147483648 {
 		return -2147483648
 	}
-	return 2147483647
-
+	return sum
 }
--- golang/008.(字符串处理)String_to_Integer(atoi)_test.go
+++ golang/008.(字符串处理)String_to_Integer(atoi)_test.go
@@ -12,6 +12,9 @@ func TestMyAtoi(t *testing.T) {
 	assert.Equal(t, 4193, MyAtoi("4193 with words"), "4193 with words")
 	assert.Equal(t, -2147483648, MyAtoi("-91283472332"), "-91283472332")
 	assert.Equal(t, 42, MyAtoi("42"), "42")
+	assert.Equal(t, 0, MyAtoi(""), "")
+	assert.Equal(t, 0, MyAtoi(" "), " ")
 	assert.Equal(t, -42, MyAtoi("   -42"), "-42")
 	assert.Equal(t, 0, MyAtoi("words and 987"), "words and 987")
+	assert.Equal(t, 12345678, MyAtoi("  0000000000012345678"), "  0000000000012345678")
 }
