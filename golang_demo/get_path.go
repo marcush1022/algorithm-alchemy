981bacb2b1eda5a6ed80d771d177c221f2882fd6
marcush <wasd666@live.com>
2021-10-26 15:28:42 +0800

    add

--- golang_demo/get_path.go
+++ golang_demo/get_path.go
@@ -7,13 +7,24 @@ import (
 "fmt"
 )
 
-const MAXVEX int = 9
+const MAXVEX int = 6
 const MAXWEIGHT int = 1000
 
-var shortTablePath = [MAXVEX]int{MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT}
+//var shortTablePath = [MAXVEX]int{MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT}
+var shortTablePath = [MAXVEX]int{MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT}
+
+var NodeWeights = map[int]int{
+	0:10,
+	1: 20,
+	2: 30,
+	3: 40,
+	4: 15,
+	5: 20,
+}
+
 
 func GetPath() {
-	graph := NewGraph()
+	graph := NewGraph2()
 
 	var TablePathMin int       //存放shortTablePath中,未遍历的最小结点的值
 	var Vx int                 //存放shortTablePath中,未遍历的最小结点的下标
@@ -39,8 +50,8 @@ func GetPath() {
 		}
 		isgetPath[Vx] = true
 		for j := 0; j < len(graph); j++ {
-			if !isgetPath[j] && TablePathMin+graph[Vx][j] < shortTablePath[j] {
-				shortTablePath[j] = TablePathMin + graph[Vx][j]
+			if !isgetPath[j] && TablePathMin+graph[Vx][j]+NodeWeights[j] < shortTablePath[j] {
+				shortTablePath[j] = TablePathMin + graph[Vx][j]+NodeWeights[j]
 			}
 		}
 
@@ -55,25 +66,42 @@ func GetPath() {
 
 }
 
-func NewGraph() [MAXVEX][MAXVEX]int {
+//func NewGraph() [MAXVEX][MAXVEX]int {
+//	var graph [MAXVEX][MAXVEX]int
+//	var v0 = [MAXVEX]int{0, 1, 5, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT}
+//	var v1 = [MAXVEX]int{1, 0, 3, 7, 5, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT}
+//	var v2 = [MAXVEX]int{5, 3, 0, MAXWEIGHT, 1, 7, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT}
+//	var v3 = [MAXVEX]int{MAXWEIGHT, 7, MAXWEIGHT, 0, 2, MAXWEIGHT, 3, MAXWEIGHT, MAXWEIGHT}
+//	var v4 = [MAXVEX]int{MAXWEIGHT, 5, 1, 2, 0, 3, 6, 9, MAXWEIGHT}
+//	var v5 = [MAXVEX]int{MAXWEIGHT, MAXWEIGHT, 7, MAXWEIGHT, 3, 0, MAXWEIGHT, 5, MAXWEIGHT}
+//	var v6 = [MAXVEX]int{MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, 3, 6, MAXWEIGHT, 0, 2, 7}
+//	var v7 = [MAXVEX]int{MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, 9, 5, 2, 0, 4}
+//	var v8 = [MAXVEX]int{MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, 7, 4, 0}
+//	graph[0] = v0
+//	graph[1] = v1
+//	graph[2] = v2
+//	graph[3] = v3
+//	graph[4] = v4
+//	graph[5] = v5
+//	graph[6] = v6
+//	graph[7] = v7
+//	graph[8] = v8
+//	return graph
+//}
+
+func NewGraph2() [MAXVEX][MAXVEX]int {
 	var graph [MAXVEX][MAXVEX]int
-	var v0 = [MAXVEX]int{0, 1, 5, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT}
-	var v1 = [MAXVEX]int{1, 0, 3, 7, 5, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT}
-	var v2 = [MAXVEX]int{5, 3, 0, MAXWEIGHT, 1, 7, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT}
-	var v3 = [MAXVEX]int{MAXWEIGHT, 7, MAXWEIGHT, 0, 2, MAXWEIGHT, 3, MAXWEIGHT, MAXWEIGHT}
-	var v4 = [MAXVEX]int{MAXWEIGHT, 5, 1, 2, 0, 3, 6, 9, MAXWEIGHT}
-	var v5 = [MAXVEX]int{MAXWEIGHT, MAXWEIGHT, 7, MAXWEIGHT, 3, 0, MAXWEIGHT, 5, MAXWEIGHT}
-	var v6 = [MAXVEX]int{MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, 3, 6, MAXWEIGHT, 0, 2, 7}
-	var v7 = [MAXVEX]int{MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, 9, 5, 2, 0, 4}
-	var v8 = [MAXVEX]int{MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, 7, 4, 0}
-	graph[0] = v0
-	graph[1] = v1
-	graph[2] = v2
-	graph[3] = v3
-	graph[4] = v4
-	graph[5] = v5
-	graph[6] = v6
-	graph[7] = v7
-	graph[8] = v8
+	var v0a = [MAXVEX]int{0, 0, 0, 0, MAXWEIGHT, MAXWEIGHT}
+	var v1b = [MAXVEX]int{MAXWEIGHT, 0, MAXWEIGHT, 0, 0, MAXWEIGHT}
+	var v2c = [MAXVEX]int{MAXWEIGHT, MAXWEIGHT, 0, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT}
+	var v3d = [MAXVEX]int{MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, 0, MAXWEIGHT, MAXWEIGHT}
+	var v4e = [MAXVEX]int{MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, 0, 0}
+	var v5f = [MAXVEX]int{MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, MAXWEIGHT, 0}
+	graph[0] = v0a
+	graph[1] = v1b
+	graph[2] = v2c
+	graph[3] = v3d
+	graph[4] = v4e
+	graph[5] = v5f
 	return graph
 }
---
+++ golang_demo/get_path_2_test.go
@@ -0,0 +1,10 @@
+// package golang_demo
+// name get_path_test_2
+
+package golang_demo
+
+import "testing"
+
+func TestGetPath2(t *testing.T) {
+	GetPath2()
+}
