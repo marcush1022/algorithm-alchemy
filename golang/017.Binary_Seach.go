b665394b2bfe17d8c46ec4bf289a1bf42363578e
marcush <wasd666@live.com>
2022-01-24 14:36:46 +0800

    binary search

---
+++ golang/017.Binary_Seach.go
@@ -0,0 +1,19 @@
+// package golang
+// name 017.Binary_Seach
+
+package golang
+
+func binarySearch(nums []int, target int) int {
+	low, high := 0, len(nums)-1
+	for low <= high {
+		mid := low + (high-low)>>1
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return -1
+}
